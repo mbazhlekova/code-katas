ac0d7faf00544c9b7b4ce1672e48c1a3ad3fe152
Maria Bazhlekova <mbazhlekova@newrelic.com>
2021-10-13 10:19:07 -0500

    feat: add anagrams and valid parentheses

---
+++ go/anagrams.go
@@ -0,0 +1,20 @@
+package anagrams
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) { return false }
+	hash := make(map[rune]int)
+	for _, char := range s {
+		hash[char]++
+	}
+
+	for _, char := range t {
+		hash[char]--
+	}
+
+	for _, value := range hash {
+		if value != 0 {
+			return false
+		}
+	}
+	return true
+}
---
+++ go/valid_parentheses.go
@@ -0,0 +1,21 @@
+package parentheses
+
+import "github.com/golang-collections/collections/stack"
+
+func isValid(s string) bool {
+    stack := stack.New()
+
+		for _, char := range s {
+			if char == '(' {
+				stack.Push(')')
+			} else if (char == '[') {
+				stack.Push(']')
+			} else if (char == '{') {
+				stack.Push('}')
+			} else if char != stack.Pop() {
+					break
+			}
+		}
+		return stack.Len() == 0
+}
+
